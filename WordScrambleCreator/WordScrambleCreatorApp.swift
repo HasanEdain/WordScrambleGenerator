8a43f2402e05b3d080f53d9181090aae847c1e5e
Hasan Edain <hasane@npcunlimited.com>
2024-09-30 19:39:09 -0700

    Initial Commit

---
+++ WordScrambleCreator/WordScrambleCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleCreatorApp.swift
+//  WordScrambleCreator
+//
+//  Created by Hasan Edain on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScrambleCreatorTests/WordScrambleCreatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleCreatorTests.swift
+//  WordScrambleCreatorTests
+//
+//  Created by Hasan Edain on 9/30/24.
+//
+
+import Testing
+@testable import WordScrambleCreator
+
+struct WordScrambleCreatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
